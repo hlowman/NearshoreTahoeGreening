42ea670455362c5c7404d8b8de29414c878e1338
Heili Lowman <heili.lowman@gmail.com>
2024-05-31 16:17:49 -0400

    Worked on dynamic time warping approach for Stage I and Stage II of nearshore Tahoe data.

---
+++ R/ts_modeling/dtw_2023.R
@@ -0,0 +1,78 @@
+# Dynamic Time Warping Stage II Script
+# Authors: Heili E. Lowman
+# Date Created: 2024-05-31
+
+# ---------------------------- README ---------------------------------
+# The following script will fit data from the second stage of
+# the project using the clustered dynamic time warping approach.
+
+#### SETUP ####
+
+# Load packages.
+library(lubridate)
+library(tidyverse)
+library(data.table)
+library(here)
+library(dtwclust)
+
+# Load data.
+data <- readRDS("data_working/do_data_2023_dailylist_053124.rds")
+
+#### TIDY ####
+
+# Must include ONLY values in the input dataset.
+# And applying scale-transform.
+data_DO <- lapply(data, function(x) {x$scaled_DO_mg_L})
+
+#### MODEL FIT ####
+
+# Perform clustered DTW on data from February through
+# September 2023 on either side of the lake and all replicates 
+# across 3m, 10m, and 20m water depths.
+dtw_2_12 <- tsclust(data_DO, 
+                    
+                    # partitional - ts may only belong to one group
+                    # fuzzy - ts may belong partially to multiple groups
+                    type="partitional", 
+                    
+                    # 2L - 2 state hypothesis (biology vs. physics)
+                    # 12L - 12 state hypothesis (independent site behavior)
+                    k = 2L:12L, # 10 possible
+                    
+                    # dtw_basic - faster distance measure per:
+                    # https://www.rdocumentation.org/packages/dtwclust/versions/5.5.12/topics/tsclust
+                    distance="dtw_basic", 
+                    
+                    # dba - DTA barycenter averaging which "iteratively refines
+                    # cluster membership until convergence criteria are met"
+                    # per Johnson et al. 2024
+                    centroid="dba")
+# Only took about a minute or so.
+
+# Examine cluster validity indices.
+# Now, this takes longer than the model fit (~ 3 minutes).
+dtw_results <- lapply(dtw_2_12, cvi)
+
+dtw_results_df <- as.data.frame(dtw_results,
+                                col.names = c("L2", "L3", "L4",
+                                              "L5", "L6", "L7",
+                                              "L8", "L9", "L10",
+                                              "L11", "L12"))
+
+dtw_results_df <- t(dtw_results_df)
+
+# Want to:
+# Maximize SIL
+# Maximize SF
+# Maximize CH
+# Minimize DB
+# Minimize DBstar
+# Maximize D
+# Minimize COP
+
+# Examine most parsimonious output.
+plot(dtw_2_12[[1]]) # by most measures
+plot(dtw_2_12[[3]]) # by D measure
+plot(dtw_2_12[[10]]) # by COP measure
+
+# End of script.
---
+++ R/ts_modeling/dtw_data_prep.R
@@ -0,0 +1,95 @@
+# Dynamic Time Warping Data Prep Script
+# Authors: Heili E. Lowman
+# Date Created: 2024-05-31
+
+# ---------------------------- README ---------------------------------
+# The following script will prep data to be fit by the 
+# dynamic time warping approach.
+
+#### SETUP ####
+
+# Load packages.
+library(lubridate)
+library(tidyverse)
+library(data.table)
+library(here)
+
+# Load data.
+data <- readRDS("R/ts_modeling/shiny/do_sat_shiny_010924.rds")
+
+#### TIDY ####
+
+# STEP 1 - Assign daily indices.
+data_hourly <- data %>%
+  mutate(date = date(Pacific_Standard_Time),
+         hour = hour(Pacific_Standard_Time)) %>%
+  group_by(site, location, replicate, date, hour) %>%
+  summarize(DO_mg_L = mean(Dissolved_O_mg_L, na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(index = 0)
+
+# Creating a function that can be applied by grouping.
+indexFUN <- function(x){
+  
+  for(i in 2:nrow(x)){
+    # if the time is equal to 4am
+    if(x$hour[i] == 4){
+      # the index value is one greater than the hour before
+      x$index[i] = x$index[(i-1)] + 1
+      # if the time is any other hour,
+    } else {
+      # the index value is the same as the hour before
+      x$index[i] = x$index[(i-1)]
+    }
+  }
+  
+  return(x)
+  
+}
+
+# See example grouping here: https://dplyr.tidyverse.org/reference/group_map.html
+data_indexed <- data_hourly %>%
+  group_by(site, location, replicate) %>%
+  group_modify(~ indexFUN(.x)) %>%
+  ungroup()
+# Looks like this worked!
+
+# Remove days with less than 24 hours of measurements.
+data_indexed_complete <- data_indexed %>%
+  group_by(site, location, replicate, index) %>%
+  mutate(count = n()) %>%
+  ungroup() %>%
+  filter(count == 24)
+
+# And create a new character column by which to split apart
+# all unique days.
+data_indexed_complete <- data_indexed_complete %>%
+  mutate(uniqueID = paste(site, location, replicate, 
+                          index, sep="_"))
+
+# And split into 2021/2022 and 2023 datasets.
+data_2022 <- data_indexed_complete %>%
+  # only use data prior to installing of cinderblocks at
+  # far from stream sites
+  filter(date < ymd("2023-02-12")) %>%
+  # and scale DO values
+  mutate(scaled_DO_mg_L = scale(DO_mg_L))
+
+data_2023 <- data_indexed_complete %>%
+  # only use data after installation of cinderblocks at
+  # far from stream sites
+  filter(date >= ymd("2023-02-12")) %>%
+  # and scale DO values
+  mutate(scaled_DO_mg_L = scale(DO_mg_L))
+
+# Finally, split into lists based on unique IDs.
+data_2022_l <- split(data_2022, data_2022$uniqueID)
+data_2023_l <- split(data_2023, data_2023$uniqueID)
+
+#### EXPORT ####
+
+# Save out these datasets for use in analyses.
+# saveRDS(data_2022_l, "data_working/do_data_2022_dailylist_053124.rds")
+# saveRDS(data_2023_l, "data_working/do_data_2023_dailylist_053124.rds")
+
+# End of script.
