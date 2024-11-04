af031e6214ccac2a26eb344994c12d31851d24b5
Heili Lowman <heili.lowman@gmail.com>
2024-11-04 10:47:21 -0500

    Exported results from hierarchical model fit. Next step is to determine what is the most helpful figure/value to export for the manuscript.

--- R/ts_modeling/posthoc_analysis.R
+++ R/ts_modeling/posthoc_analysis.R
@@ -101,14 +101,15 @@ data_2022_multireg <- data_2022_select %>%
 
 ##### Model Fit #####
 
-# Fit multilevel multinomial logistic regression model.
 # Note, after much googling, it seems no packages support
 # the frequentist format as well as the Bayesian ones,
 # so defaulting to a Bayesian approach here only for that
 # reason, not because we necessarily have priors for
 # any of the covariates.
-fit_2022 <- brm(group ~ scale_light + scale_wind + scale_q 
-                + (1|site/sensor),
+
+# Fit multilevel multinomial logistic regression model.
+fit_2022 <- brm(group ~ scale_light + scale_wind + scale_q
+                + (1|site/sensor), # nested random effect
                 data = data_2022_multireg,
                 # specify categorical if vectorized data
                 # specify multinomial if data is a matrix
@@ -116,29 +117,109 @@ fit_2022 <- brm(group ~ scale_light + scale_wind + scale_q
 
 # Save model fit.
 # saveRDS(fit_2022,
-#         "data_model_outputs/multreg_2022_110124.rds")
+#         "data_model_outputs/brms_2022_110424.rds")
 
-# Examine model fit in format that adds p-values.
-fit_df <- broom::tidy(fit_2022, conf.int = TRUE)
+##### Diagnostics #####
 
-# In order to better translate results, will exponentiate
-# coefficients.
-fit_df$expB <- exp(fit_df$estimate)
+# Examine model fit.
+summary(fit_2022)
+# Despite 57 divergent transitions, Rhats look good!
 
-# And exponentiate separately to work with function below.
-fit_exp <- exp(coef(fit_2022))
+plot(fit_2022, variable = c("b_muCluster1_scale_light",
+                            "b_muCluster1_scale_wind",
+                            "b_muCluster1_scale_q",
+                            "b_muCluster2_scale_light",
+                            "b_muCluster2_scale_wind",
+                            "b_muCluster2_scale_q"))
+# Chain mixing looking good!
 
-# Examine in another table format. 
-stargazer(fit_df, type = "text", coef = list(fit_exp))
+# Be sure no n_eff are < 0.1
+mcmc_plot(fit_2022, type = "neff")
 
-# For example, the odds of a day being assigned to Cluster 1
-# increases relative to Neither by 132% for each unit increase
-# in scale_q. And the odds of a day being assigned to
-# Cluster 2 increases relative to Neither by 200% for each
-# unit increase in scale_light.
-# Another way of putting it is, for each unit increase in
-# light values, you are 0.20 times more likely to stay in
-# the Cluster 1 category as compared to Neither category
-# (so the risk/odds is 80% lower).
+# Examine relationships for each predictor.
+plot(conditional_effects(fit_2022, effects = "scale_light",
+                         categorical = TRUE))
+plot(conditional_effects(fit_2022, effects = "scale_wind",
+                         categorical = TRUE))
+plot(conditional_effects(fit_2022, effects = "scale_q",
+                         categorical = TRUE))
+
+# Appears mean daily q is greatest in Cluster 1,
+# daily change in light is greatest in Cluster 2,
+# and effect of wind is not as clearly delineated.
+
+##### Visualization #####
+
+# Examine the posterior data.
+post_data <- mcmc_intervals_data(fit_2022,
+                                 point_est = "median", # default = "median"
+                                 prob = 0.66, # default = 0.5
+                                 prob_outer = 0.95) # default = 0.9
+
+View(post_data)
+
+(fig_custom <- ggplot(post_data %>%
+                          filter(parameter %in% c("b_muCluster1_scale_light",
+                                                  "b_muCluster1_scale_wind",
+                                                  "b_muCluster1_scale_q",
+                                                  "b_muCluster2_scale_light",
+                                                  "b_muCluster2_scale_wind",
+                                                  "b_muCluster2_scale_q")) %>%
+                          mutate(par_f = factor(parameter, 
+                                                levels = c("b_muCluster1_scale_light",
+                                                           "b_muCluster2_scale_light",
+                                                           "b_muCluster1_scale_wind",
+                                                           "b_muCluster2_scale_wind",
+                                                           "b_muCluster1_scale_q",
+                                                           "b_muCluster2_scale_q"))), 
+                        aes(x = m, y = par_f, color = par_f)) +
+    geom_linerange(aes(xmin = ll, xmax = hh),
+                   size = 2, alpha = 0.5) +
+    geom_point(size = 3) +
+    vline_at(v = 0) +
+    scale_x_continuous(breaks = c(-1, 0, 1)) +
+    labs(x = "Posterior Estimates",
+         y = "Predictors") +
+    scale_y_discrete(labels = c("b_muCluster1_scale_light" = "Cluster 1 Light",
+                                "b_muCluster1_scale_wind" = "Cluster 1 Windspeed",
+                                "b_muCluster1_scale_q" = "Cluster 1 Discharge",
+                                "b_muCluster2_scale_light" = "Cluster 2 Light",
+                                "b_muCluster2_scale_wind" = "Cluster 2 Windspeed",
+                                "b_muCluster2_scale_q" = "Cluster 2 Discharge")) +
+    theme_bw() +
+    scale_color_manual(values = c("#FABA39","#1AE4B6", 
+                                  "#FABA39","#1AE4B6",
+                                  "#FABA39","#1AE4B6")) +
+    theme(text = element_text(size = 10),
+          legend.position = "none"))
+
+# ggsave(fig_custom,
+#        filename = "figures/brms_2022_110424.jpg",
+#        height = 8,
+#        width = 12,
+#        units = "cm")
+
+# STOPPED HERE.
+
+# Make dataframe with all possible combinations of values.
+all_data <- data_2022_multireg %>%
+  expand(scale_light, scale_wind, scale_q, site)
+
+# And generate predicted draws.
+all_predictions <- fit_2022 %>%
+  epred_draws(newdata = all_data)
+
+# Note for next time - the above made too large of a dataset,
+# so I'll need to make epred draws similar to how I did for the
+# biomass project and take it from there...
+# see: https://github.com/hlowman/ContinentalRiverBiomass/blob/main/code/beartooth_spring23/13_Posthoc_analyses.R
+
+# Online Resources:
+# ~ Frequentist Multinomial Regression ~
+# https://www.princeton.edu/~otorres/LogitR101.pdf
+# https://bookdown.org/sarahwerth2024/CategoricalBook/multinomial-logit-regression-r.html
+# https://www.carlosivanrodriguez.com/guides/statistics/logistic-regression/multinomial-regression/
+# ~ Bayesian Multinomial Regression ~
+# https://www.andrewheiss.com/blog/2023/08/12/conjoint-multilevel-multinomial-guide/#tldr-moral-of-the-story
 
 # End of script.
